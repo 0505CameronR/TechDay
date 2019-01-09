1e2960ae3b765fcc21b48796e077b29553ff9678
0505cameronr <0505cameronr@gcfe.net>
2019-01-09 11:58:34 +0000

    Removing Un-Needed lines

--- TumainiFund/src/app/shared/user/user.service.ts
+++ TumainiFund/src/app/shared/user/user.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http, Headers, Response } from "@angular/http";
-import { Observable } from "rxjs";
-import { catchError, map, tap } from "rxjs/operators";
+import { Http } from "@angular/http";
 
 import { Kinvey } from "kinvey-nativescript-sdk";
 
@@ -12,16 +10,6 @@ export class UserService {
     constructor(private http: Http) { }
 
     register(user: User) {
-        // return this.http.post(
-        //     Config.apiUrl + "user/" + Config.appKey,
-        //     JSON.stringify({
-        //         username: user.username,
-        //         password: user.password
-        //     }),
-        //     { headers: this.getCommonHeaders() }
-        // ).pipe(
-        //     catchError(this.handleErrors)
-        // );
         return new Promise((resolve, reject) => {
             Kinvey.User.logout()
               .then(() => {
@@ -34,20 +22,6 @@ export class UserService {
     }
 
     login(user: User) {
-        // return this.http.post(
-        //     Config.apiUrl + "user/" + Config.appKey + "/login",
-        //     JSON.stringify({
-        //         username: user.username,
-        //         password: user.password
-        //     }),
-        //     { headers: this.getCommonHeaders() }
-        // ).pipe(
-        //     map(response => response.json()),
-        //     tap(data => {
-        //         Config.token = data._kmd.authtoken
-        //     }),
-        //     catchError(this.handleErrors)
-        // );
         return new Promise((resolve, reject) => {
             Kinvey.User.logout()
               .then(() => {
@@ -59,17 +33,6 @@ export class UserService {
             });
     }
 
-    // getCommonHeaders() {
-    //     let headers = new Headers();
-    //     headers.append("Content-Type", "application/json");
-    //     headers.append("Authorization", Config.authHeader);
-    //     return headers;
-    // }
-
-    // handleErrors(error: Response) {
-    //     console.log(JSON.stringify(error.json()));
-    //     return Observable.throw(error);
-    // }
     handleErrors(error: Kinvey.BaseError) {
         console.error(error.message);
         return error.message;
